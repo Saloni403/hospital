cb297e6980b029f78ebb9477a28e1b4c1fcbe055
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2026-02-19 17:40:25 +0530

    first commit

---
+++ web/js/script.js
@@ -0,0 +1,7 @@
+let Servicesdropdown = document.querySelector(".Servicesdropdown")
+let sublist = document.querySelector(".sublist")
+
+Servicesdropdown.addEventListener("click", ()=>{
+    sublist.classList.toggle("active")
+
+})
