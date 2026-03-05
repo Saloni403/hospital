4e1cab8859a04a367b9925768c4a524e44134bbd
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2026-03-05 15:21:18 +0530

    done

--- web/js/script.js
+++ web/js/script.js
@@ -37,3 +37,33 @@ document.querySelector(".menusublist").classList.toggle("active")
 form.classList.remove("active")
 document.body.style.overflow="scroll"
         })
+
+        // chatbot
+    let chatbot = document.querySelector(".chatbot")
+let chatbotimg = chatbot.querySelector(".mainimg")
+let chattingbox = document.querySelector(".box1")
+let closingchatbot = document.querySelector(".closingchatbot")
+
+chatbotimg.addEventListener("click", ()=>{
+
+    if(chattingbox.style.display === "block"){
+        // close chatbot
+        chattingbox.style.display="none"
+        chatbotimg.src="web/images/assistant.png"
+        chatbot.style.animation="jumping 2s linear infinite"
+         document.body.style.overflow="auto"
+    }else{
+        // open chatbot
+        chatbotimg.src="web/images/close.png"
+        chatbot.style.animation="none"
+        chattingbox.style.display="block"
+        document.body.style.overflow="hidden"
+    }
+
+})
+
+closingchatbot.addEventListener("click", ()=>{
+    chattingbox.style.display="none"
+    chatbotimg.src="web/images/assistant.png"
+    chatbot.style.animation="jumping 2s linear infinite"
+})
